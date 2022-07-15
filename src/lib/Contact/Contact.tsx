74b34965f191e51b5d0d310252b07f04034bc863
Dominic Fournier <me@dominicfournier.com>
2022-07-15 17:31:21 -0400

    Added configurable placeholder for contact, added newsletter subscribe button action.

--- src/lib/Contact/Contact.tsx
+++ src/lib/Contact/Contact.tsx
@@ -10,29 +10,38 @@ import Button from "../Button";
 const Contact: React.FC<ContactProps> = ({
   newsletter,
   coords,
-  onSubscribe,
-}) => (
-  <div data-testid="Contact" className="oreus-contact">
-    <div className="oreus-contact-newsletter">
-      <h3>{newsletter.title}</h3>
-      <p>{newsletter.description}</p>
-      <div className="oreus-contact-newsletter-action">
-        <TextInput placeholder="Courriel" />
-        <Button
-          label="M'inscrire"
-          variant="contained"
-          color="light"
-          className="oreus-contact-btn"
-        />
+  onSubscribe = () => {},
+}) => {
+  const [email, setEmail] = React.useState("");
+
+  return (
+    <div data-testid="Contact" className="oreus-contact">
+      <div className="oreus-contact-newsletter">
+        <h3>{newsletter.title}</h3>
+        <p>{newsletter.description}</p>
+        <div className="oreus-contact-newsletter-action">
+          <TextInput
+            placeholder={newsletter.inputPlaceholder}
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <Button
+            label="M'inscrire"
+            variant="contained"
+            color="light"
+            className="oreus-contact-btn"
+            onClick={() => onSubscribe(email)}
+          />
+        </div>
+      </div>
+      <div className="oreus-contact-coords">
+        <h3>{coords.title}</h3>
+        <p>{coords.address}</p>
+        <p>{coords.phone}</p>
+        <p>{coords.email}</p>
       </div>
     </div>
-    <div className="oreus-contact-coords">
-      <h3>{coords.title}</h3>
-      <p>{coords.address}</p>
-      <p>{coords.phone}</p>
-      <p>{coords.email}</p>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Contact;
--- src/lib/Contact/Contact.types.ts
+++ src/lib/Contact/Contact.types.ts
@@ -1,8 +1,17 @@
 // Generated with util/create-component.js
 export interface ContactProps {
-  newsletter: 
-  coords,
-  onSubscribe,
+  newsletter: {
+    title?: string;
+    description?: string;
+    inputPlaceholder?: string;
+  }
+  coords: {
+    title?: string;
+    address?: string;
+    phone?: string;
+    email?: string;
+  }
+  onSubscribe?: any;
 }
 
 export interface ContactNewsletter
